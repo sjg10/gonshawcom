4e52678639a7fe835f20abd094d8ebbbd11a7073
Sam Gonshaw <sam@gonshaw.com>
2017-02-11 18:06:51 +0000

    Got software automated

--- content/models.py
+++ content/models.py
@@ -8,6 +8,8 @@ from django.conf import settings
 class Post(models.Model):
     post_title = models.CharField(max_length=200)
     post_text = models.TextField()
+    choices = (('m','Maths'),('s','Software'))
+    post_topic = models.CharField(max_length=10,choices=choices, default='m')
     post_image = models.ImageField(
             upload_to=settings.UPLOADS_URL[1:] + "pics/",
             default =settings.UPLOADS_URL[1:] + 'pics/no-img.jpg')
--- content/views.py
+++ content/views.py
@@ -4,10 +4,8 @@ from .models import Post
 
 # Create your views here.
 def index(request):
-    math_posts = Post.objects.all()
-    context = {'math_posts': math_posts}
+    math_posts = Post.objects.filter(post_topic='m')
+    software_posts = Post.objects.filter(post_topic='s')
+    context = {'math_posts': math_posts,
+        'software_posts': software_posts}
     return render(request, 'content/index.html', context)
-
-def detail(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'content/post.html', {'post': post})
