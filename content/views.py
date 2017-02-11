23bc5195ddaaaa46306ad8262187915f6abf5460
Sam Gonshaw <sam@gonshaw.com>
2017-02-11 13:51:12 +0000

    Import old site

--- content/views.py
+++ content/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    context = {}
+    return render(request, 'content/index.html', context)
--- gonshawcom/settings.py
+++ gonshawcom/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'content.apps.ContentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
